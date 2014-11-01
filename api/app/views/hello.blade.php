f8fb8cebb29965ffe41668ceebf8bc503fe0a55b
fabianoP <fabiano.pallonetto@gmail.com>
2014-11-01 09:18:37 +0000

    add home page

--- api/app/views/hello.blade.php
+++ api/app/views/hello.blade.php
@@ -1,20 +1,163 @@
 @extends('layouts.default')
 
 @section('content') 
-<div class="portlet light ">
-						<div class="portlet-title">
-							<div class="caption">
-								<i class="icon-bar-chart font-green-sharp hide"></i>
-								<span class="caption-subject font-green-sharp bold uppercase">Energy Plus made Web Friendly </span>
+
+			
+			<!-- BEGIN PAGE HEADER-->
+			<div class="row">
+				<div class="col-md-12">
+					<!-- BEGIN PAGE TITLE & BREADCRUMB-->
+					<h3 class="page-title">
+					A RestFUL API to control a smart grid-ready residential building modelled in Energy Plus
+					</h3>
+					<ul class="page-breadcrumb breadcrumb">
+						
+				
+					</ul>
+					<!-- END PAGE TITLE & BREADCRUMB-->
+				</div>
+			</div>
+			<!-- END PAGE HEADER-->
+			<!-- BEGIN PAGE CONTENT-->
+			<div class="row">
+				<div class="col-md-12 news-page blog-page">
+					<div class="row">
+						<div class="col-md-9 blog-tag-data">
+							<img src="{{ URL::asset('assets/images/realimageopt.jpg')  }}" class="img-responsive" alt="">
+						
+							<div class="news-item-page">
+								<div class="space20">
 							</div>
+								<blockquote class="hero">
+									<p>
+										 The objective of this work is to develop a building test environment that will allow comparing and benchmarking different control algorithms, storing the results in a database and display comparison benchmark graphs. 
+								
+									</p>
+									<p>
+									 Building simulation software allows expert users to accurately evaluate the energy performances of buildings. Coupling a building simulation environment with an advanced control system requires additional and specialised knowledge of both control development and building simulation software modelling. 
+							
+								</p>
+								<p>
+									Simapi offer the possibility to validate and benchmark sophisticated control algorithms on detailed building software models with a reduced modelling experience and knowledge. 
+								</p>
+									
+								</blockquote>
+								
+							</div>
+							<hr>
+							
+							
 						</div>
-						<div class="portlet-body">
-							 <p class="lead"> The goal of this project is to develop a simulation actor and connect to a database to connect a building energy management system via building simulation software and benchmark the performances and a restful API.</p>
-	<p class="lead"> Building simulation software allows expert users to accurately evaluate the energy performances of buildings. Coupling the building simulation environment with an advanced control system requires additional and specialised knowledge of both control development and building simulation software modelling. There is a gap between developers who build software controls for buildings (like home automation control systems) and building simulation software experts. The possibility to validate and benchmark sophisticated control algorithms on detailed building software models with a reduced modelling experience and knowledge could benefit building users, energy system designers and software engineers.</p>
-    <p class="lead"> The objective is to develop a smart building test environment that will allow to compare and benchmark different control algorithms and store the results in a database a display comparison benchmark graphs.</p>
-  
+						<div class="col-md-3">
+							<h3>Project Timeline</h3>
+							<div class="top-news">
+								<a href="#" class="btn green">
+								<span>
+								SimApi V 0.1 is online</span>
+								<em>Posted on: Nov 1, 2014</em>
+								<em>
+								<i class="fa fa-tags"></i>
+								News, Energy, Simulation </em>
+								<i class="fa fa-music top-news-icon"></i>
+								</a>
+								<a href="#" class="btn yellow">
+								<span>
+								API spec released </span>
+								<em>Posted on: Sep 1, 2014</em>
+								<em>
+								<i class="fa fa-tags"></i>
+								Education, Apiary, API </em>
+								<i class="fa fa-book top-news-icon"></i>
+								</a>
+								<a href="#" class="btn red">
+								<span>
+								Project Started </span>
+								<em>Posted on: June 3, 2014</em>
+								<em>
+								<i class="fa fa-tags"></i>
+								Idea, API, SimApi </em>
+								<i class="fa fa-briefcase top-news-icon"></i>
+								</a>
+								<a href="#" class="btn blue">
+								<span>
+								SimApi Idea </span>
+								<em>Posted on: Feb 13, 2014</em>
+								<em>
+								<i class="fa fa-tags"></i>
+								Idea, Interface, API </em>
+								<i class="fa fa-globe top-news-icon"></i>
+								</a>
+							</div>
+							<div class="space20">
+							</div>
+							<h3>Related Tags</h3>
+							<ul class="list-inline sidebar-tags">
+								<li>
+									<a href="#">
+									<i class="fa fa-tags"></i> Energy Plus </a>
+								</li>
+								<li>
+									<a href="#">
+									<i class="fa fa-tags"></i> Simulation </a>
+								</li>
+								<li>
+									<a href="#">
+									<i class="fa fa-tags"></i> Building </a>
+								</li>
+								<li>
+									<a href="#">
+									<i class="fa fa-tags"></i> Control </a>
+								</li>
+								<li>
+									<a href="#">
+									<i class="fa fa-tags"></i> Algorithms </a>
+								</li>
+								<li>
+									<a href="#">
+									<i class="fa fa-tags"></i> RestFul API  </a>
+								</li>
+								<li>
+									<a href="#">
+									<i class="fa fa-tags"></i> Education </a>
+								</li>
+								<li>
+									<a href="#">
+									<i class="fa fa-tags"></i> Research </a>
+								</li>
+								<li>
+									<a href="#">
+									<i class="fa fa-tags"></i> Demand Response </a>
+								</li>
+								<li>
+									<a href="#">
+									<i class="fa fa-tags"></i> Weather </a>
+								</li>
+								<li>
+									<a href="#">
+									<i class="fa fa-tags"></i> Forecast </a>
+								</li>
+								<li>
+									<a href="#">
+									<i class="fa fa-tags"></i> Analytics </a>
+								</li>
+								<li>
+									<a href="#">
+									<i class="fa fa-tags"></i> Interface </a>
+								</li>
+								<li>
+									<a href="#">
+									<i class="fa fa-tags"></i> BCVTB </a>
+								</li>
+							</ul>
+							
+							<div class="space20">
+							</div>
+									
 						</div>
-</div>
+					</div>
+				</div>
+			</div>
+			<!-- END PAGE CONTENT-->
 
 
       
